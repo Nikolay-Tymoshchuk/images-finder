3673ecdb364d1856fb39834e07c38156db9f3bd7
Nikolay Tymoshchuk <saintnikfreak@gmail.com>
2022-07-11 01:15:50 +0300

    done

--- src/components/button/Button.styled.jsx
+++ src/components/button/Button.styled.jsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 export const Loadmore = styled.button`
   padding: 13px 16px;
   border-radius: 5px;
-  background-color: #3f51b5;
+  background-color: var(--color-accent);
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
   text-align: center;
   display: inline-block;
@@ -26,6 +26,6 @@ export const Loadmore = styled.button`
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
   &:hover,
   &:focus {
-    background-color: #303f9f;
+    background-color: var(--color-hover);
   }
 `;
--- src/components/buttonUp/ButtonUp.styled.jsx
+++ src/components/buttonUp/ButtonUp.styled.jsx
@@ -7,14 +7,14 @@ export const Button = styled(ImArrowUp)`
   right: 0;
   cursor: pointer;
   color: #fff;
-  background: #3f51b5;
+  background: var(--color-accent);
   padding: 10px;
   transform: translate(-50%, -50%);
   border-radius: 10%;
   transition: 175ms ease-in-out;
 
   &:hover {
-    background: #303f9f;
+    background: var(--color-hover);
     transform: translate(-50%, -60%) scale(1.1);
     transition: 350ms ease-in-out;
   }
